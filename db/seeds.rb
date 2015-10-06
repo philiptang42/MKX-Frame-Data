588f3bab0fa97fc0bc2666e0d016f181d21a15fb
philiptang42 <philiptang42@gmail.com>
2015-10-06 15:11:51 -0400

    basic index page functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+characters = [
+  ['Scorpion', 'A risen Spectre hellbent on obtaining vengeance.'],
+  ['Subzero', 'Icy Grandmaster of the Lin Kuei Clan.'],
+  ['Cassie Cage', 'Special Forces sergeant; bane of Shinnok.'],
+  ['Kitana', 'Princess of Edenia and stepdaughter to the deceased Shao Kahn.'],
+  ['Kenshi', 'A blind, telekinetic swordsman wielding the ancient Katana, Sento.']
+]
+
+characters.each do |character|
+  name, description = character
+  c = Character.find_or_create_by!(name: name, description: description)
+end
