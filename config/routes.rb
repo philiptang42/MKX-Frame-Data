a18dcbf82fab71442020f6192cc4d568fae57657
philiptang42 <philiptang42@gmail.com>
2015-10-07 16:55:06 -0400

    adding show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'characters#index'
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+  }
+  resources :characters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
