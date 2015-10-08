58eac58fb8014398f4dfbe110fe7974ac8820320
philiptang42 <philiptang42@gmail.com>
2015-10-08 15:18:50 -0400

    registration pages styled

--- db/schema.rb
+++ db/schema.rb
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 20151007160528) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
