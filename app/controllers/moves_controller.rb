70ae074152704492a50aa1a4d71c35567c8fde9d
philiptang42 <philiptang42@gmail.com>
2015-10-15 13:35:28 -0400

    comments added to move show page

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -3,7 +3,8 @@ class MovesController < ApplicationController
   def show
     @character = Character.find(params[:character_id])
     @move = @character.moves.find(params[:id])
-    
+    @comments = @move.comments
+    @comment = Comment.new
   end
 
 end
