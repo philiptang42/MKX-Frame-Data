0dd21212c4f9cf61b8c7c30695539b2a01bb10c9
philiptang42 <philiptang42@gmail.com>
2015-10-16 16:28:23 -0400

    AJAX form submission

--- app/assets/javascripts/submitcomment.js
+++ app/assets/javascripts/submitcomment.js
@@ -1,29 +1,33 @@
-$('#comment-form').on('submit', function(event){
+$('#new_comment').on('submit', function(event){
         event.preventDefault();
         var form = $(this);
         var body = $('#comment-body').val();
-        var rating = $('#comment-rating').val();
+        var rating = $('#comment_rating').val();
 
-        $.ajax({
+        var request = $.ajax({
             url: form.attr('action'),
             method: form.attr('method'),
             data: {
                 "comment": {
                 "body": body,
-                "rating": rating,
+                "rating": rating
                 }
             },
-            dataType: "json",
-            success: function(data) {
-                console.log(data);
-                var panel = $('panel.comments');
-                var rating =  '<h5="comment-rating">'+data.rating+'</h5>';
-                panel.append(rating);
-                var body = '<h2="comment-body">'+data.body+'</h2>';
-                $(':text').val('');
-            },
-            error: function(){
+            dataType: "json"});
+            request.success(function(data) {
+    var newCommentPanel = '<div class="panelz" style="padding-bottom:10px;"><div class="panel small-6 large-10 large-offset-1" style="display:block;", id="comments_panel"><p style="text-shadow: 2px 2px black">Written by' +
+    ' ' + data.user + ' ' + data.timeText + 'ago.</p><div class="panel", id="comments"><p><h5 style="text-shadow: 2px 2px black", id="c-rating">Rating:'
+      + ' ' + data.rating + '</h5></p><p><h2 style="text-shadow: 2px 2px black", id="c-body">' + data.body + '</h2></p></div></div>';
+              $(".comments-container").prepend(newCommentPanel);
+                // var div = $('div#comments.panel');
+                // var rating =  '<h5 id="c-rating">'+data.rating+'</h5>';
+                // div.append(rating);
+                // var body = '<h2 id="c-body">'+data.body+'</h2>';
+                // div.append(body);
+            });
+            request.error(function(data) {
+              debugger;
                 alert("Error!");
-            }
-        });
+            });
+
       });
