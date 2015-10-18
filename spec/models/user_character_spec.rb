e28c26fdbdaaba681fed964e4885d4819617d652
philiptang42 <philiptang42@gmail.com>
2015-10-18 02:28:37 -0400

    undo

--- spec/models/user_character_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe UserCharacter do
-  describe "associations" do
-    it { should belong_to(:user) }
-    it { should belong_to(:character) }
-  end
-end
--- spec/models/user_move_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe UserMove do
-  describe "associations" do
-    it { should belong_to(:user) }
-    it { should belong_to(:move) }
-  end
-end
