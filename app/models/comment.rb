86ed936da432aae741f83f4131cccf40be0188ad
philiptang42 <philiptang42@gmail.com>
2015-11-17 10:48:07 -0500

    ready for pull request

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,4 +8,26 @@ class Comment < ActiveRecord::Base
   validates :rating, inclusion: { in: 1..5, message: " must be between 1 to 5" }
   validates :body, presence: true
   validates :body, length: { minimum: 5 }
+
+def upvotes_score
+  upvotes = comment_votes.where(upvote: 1).count
+  if upvotes.nil?
+    return 0
+  else
+    return upvotes
+  end
+end
+
+def downvotes_score
+  downvotes = comment_votes.where(downvote: 1).count
+  if downvotes.nil?
+    return 0
+  else
+    return downvotes
+  end
+end
+
+def score
+  self.upvotes_score - self.downvotes_score
+end
 end
